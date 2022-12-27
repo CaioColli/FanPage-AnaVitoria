2e22245848ea75848b7526c3f73fa76fbc4d6072
Caio Colli <110783734+CaioColli@users.noreply.github.com>
2022-12-27 17:16:02 -0300

    Att 27/12
    New Page

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 // Menu Lateral Mobile
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+}
 
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-  
-  // Menu Lateral Mobile
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// Menu Lateral Mobile
+
+// Função para voltar para o topo
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// Função para voltar para o topo
